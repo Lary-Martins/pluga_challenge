0770d71e243b3e46abcd9112de4365e2b6f4a8cc
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-07-08 17:39:24 -0300

    test: Verifica se na renderização do app contém uma barra de busca

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -5,9 +5,15 @@ describe('Verifica se na renderização do app contém:', () => {
   render(<App />);
 
   test('Doze cards na tela', async () => {
-    const allCards = await screen.findAllByTestId('tool-card')
+    const allCards = await screen.findAllByTestId('tool-card');
     const length = 12;
 
     expect(allCards).toHaveLength(length);
+  });
+
+  test('Barra de busca', () => {
+    const searchBar = screen.getByTestId('tool-search');
+
+    expect(searchBar).toBeInTheDocument();
   });
 });
