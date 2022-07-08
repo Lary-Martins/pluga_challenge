7c258a9d726d5364cf544e83119b712c3c7c6ee1
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-07-08 15:48:25 -0300

    feat (service): define função de fetch

---
+++ src/services/getTools.tsx
@@ -0,0 +1,11 @@
+import ToolTypes from "../types/ToolTypes";
+
+const getTools = async (): Promise <ToolTypes[] | null> => {
+  const result = await fetch('https://pluga.co/ferramentas_search.json')
+    .then((response) => response.json())
+    .catch((reject) => console.log(reject));
+
+  return result;
+}
+
+export default getTools;
