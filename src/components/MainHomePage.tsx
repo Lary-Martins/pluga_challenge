cfc91eb8da64928147e662bfc094864df736ff75
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-07-08 23:45:10 -0300

    feat (components) : inicia implementação da funcionalidade de paginação

--- src/components/MainHomePage.tsx
+++ src/components/MainHomePage.tsx
@@ -5,14 +5,23 @@ import ToolTypes from "../types/ToolTypes";
 
 const MainHomePage = () => {
   const [tools, setTools] = useState<ToolTypes[] | []>([]);
+  const [currentPage, setCurrentPage] = useState<number>(0);
 
   useEffect(() => {
     async function getData () {
       const result = await getTools();
-      setTools(result) 
+      setTools(result)
     }
     getData()
   }, []) 
+
+  const handleClickPrevPage = () => {
+    setCurrentPage((prevState) => prevState - 1);
+  }
+
+  const handleClickNextPage = () => {
+    setCurrentPage((prevState) => prevState + 1);
+  }
 
   return (
     <section>
@@ -21,7 +30,27 @@ const MainHomePage = () => {
           <Card { ...tool } />
         )) 
       }
-    </section>
+    <div>
+    <button
+      type="button"
+      data-testid="prev-page"
+      disabled={ currentPage === 0 }
+      onClick={ handleClickPrevPage }
+      > 
+      Anterior
+    </button>
+
+    <span>{ currentPage }</span>
+
+    <button
+      type="button"
+      data-testid="next-page"
+      onClick={ handleClickNextPage }
+    > 
+      Próxima
+    </button>
+    </div>
+  </section>
   )
 }
 export default MainHomePage;
