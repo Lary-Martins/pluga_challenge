21b799138c6fd35c02ffc45213e3f9018143d528
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-07-24 20:38:56 -0300

    feat: implementa o componente de paginação

--- src/components/MainHomePage.tsx
+++ src/components/MainHomePage.tsx
@@ -2,12 +2,14 @@ import { useContext } from "react";
 import Card from "./Card";
 import ToolTypes from "../types/ToolTypes";
 import MyContext from "../context/MyContext";
+import Pagination from "./Pagination";
 
 const MainHomePage = () => {
   const { tools } = useContext(MyContext)
 
   return (
     <section>
+      <Pagination />
       {
         tools.map((tool: ToolTypes, _index: number) => (
           <Card {...tool} />
