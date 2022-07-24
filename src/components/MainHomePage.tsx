687b73607aaf7ceb084bda3d59be5c22595a3100
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-07-24 10:49:09 -0300

    refactor: utiliza o contexto para acessar os dados

--- src/components/MainHomePage.tsx
+++ src/components/MainHomePage.tsx
@@ -1,56 +1,20 @@
-import { useEffect, useState } from "react";
-import getTools from "../services/getTools";
+import { useContext } from "react";
 import Card from "./Card";
 import ToolTypes from "../types/ToolTypes";
+import MyContext from "../context/MyContext";
 
 const MainHomePage = () => {
-  const [tools, setTools] = useState<ToolTypes[] | []>([]);
-  const [currentPage, setCurrentPage] = useState<number>(0);
-
-  useEffect(() => {
-    async function getData () {
-      const result = await getTools();
-      setTools(result)
-    }
-    getData()
-  }, []) 
-
-  const handleClickPrevPage = () => {
-    setCurrentPage((prevState) => prevState - 1);
-  }
-
-  const handleClickNextPage = () => {
-    setCurrentPage((prevState) => prevState + 1);
-  }
+  const { tools } = useContext(MyContext)
 
   return (
     <section>
-      { 
+      {
         tools.map((tool: ToolTypes, _index: number) => (
-          <Card { ...tool } />
-        )) 
+          <Card {...tool} />
+        ))
       }
-    <div>
-    <button
-      type="button"
-      data-testid="prev-page"
-      disabled={ currentPage === 0 }
-      onClick={ handleClickPrevPage }
-      > 
-      Anterior
-    </button>
+    </section>
+  );
+};
 
-    <span>{ currentPage }</span>
-
-    <button
-      type="button"
-      data-testid="next-page"
-      onClick={ handleClickNextPage }
-    > 
-      Próxima
-    </button>
-    </div>
-  </section>
-  )
-}
 export default MainHomePage;
