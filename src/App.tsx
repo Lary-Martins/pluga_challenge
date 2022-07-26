bac681af985b622cd08d182c882300761e16c588
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-07-25 23:24:48 -0300

    chore: padroniza código

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,6 @@ function App() {
       <RouteMap />
     </MyProvider>
   );
-};
+}
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
-import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
+import {
+  Route, Routes, BrowserRouter, Navigate,
+} from 'react-router-dom';
 import Home from '../pages/Home';
 
-const RouteMap= () => {
+function RouteMap() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element= { <Home /> } />
-        <Route path="*" element= { <Navigate to="/" /> } />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default RouteMap;
