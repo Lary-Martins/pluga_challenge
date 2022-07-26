cc80917308c221b2d0be18c2ae3eac3627c72509
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-07-25 23:25:16 -0300

    fix: edita os data-testid para o teste

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,12 +4,12 @@ function Card(tool: ToolTypes) {
   const { name, icon } = tool;
 
   return (
-    <div
-      data-testid="tool-card"
-      className="tool-card"
-    >
-      <span>{name}</span>
+    <div data-testid="tool-card">
+      <span data-testid="tool-card-name">
+        {name}
+      </span>
       <img
+        data-testid="tool-card-img"
         src={icon}
         alt={name}
       />
