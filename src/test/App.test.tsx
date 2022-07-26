0217c87963cb79f905267794f146d241e66508ab
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-07-25 23:56:43 -0300

    fix: add double quotes nos data-testid

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -5,34 +5,39 @@ describe('Verifica se na renderização do app contém:', () => {
   render(<App />);
 
   test('Doze cards na tela', async () => {
-    const allCards = await screen.findAllByTestId('tool-card');
+    const allCards = await screen.findAllByTestId("tool-card");
     const length = 12;
 
     expect(allCards).toHaveLength(length);
+    expect(allCards).toBeInTheDocument();
   });
 
   test('Cada card possui uma imagem', async () => {
-    const allCards = await screen.findAllByTestId('tool-card-img');
+    const allCards = await screen.findAllByTestId("tool-card-img");
+    const length = 12;
 
+    expect(allCards).toHaveLength(length);
     expect(allCards).toBeInTheDocument();
   });
 
   test('Cada card possui o nome do app', async () => {
-    const allCards = await screen.findAllByTestId('tool-card-name');
+    const allCards = await screen.findAllByTestId("tool-card-name");
+    const length = 12;
 
+    expect(allCards).toHaveLength(length);
     expect(allCards).toBeInTheDocument();
   });
 
   test('Barra de busca', () => {
-    const searchBar = screen.getByTestId('tool-search');
-    const inputTextSearch = screen.getByTestId('tool-search-input');
+    const searchBar = screen.getByTestId("tool-search");
+    const inputTextSearch = screen.getByTestId("tool-search-input");
 
     expect(searchBar).toBeInTheDocument();
     expect(inputTextSearch).toBeInTheDocument();
   });
 
   test('Botões de paginação', () => {
-    const buttons = screen.getAllByTestId('page-button');
+    const buttons = screen.getAllByTestId("page-button");
     const length = 6;
 
     expect(buttons).toHaveLength(length);
